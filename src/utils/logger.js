c6ac90d05ff42e1fad82573b3a449f04a9803b64
Guillaume Badi <gbadi@student.42.fr>
2016-06-20 15:29:19 +0200

    logger comments

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -6,6 +6,14 @@ import chalk from 'chalk'
 const log = (c, f) => m =>
   console.log(chalk[c](`[${new Date()} - ${f}]`), m)
 
+/*
+ * const log = logger('Logger')
+ *
+ * log('Hello') // [Mon Jun 20 2016 14:53:39 GMT+0200 (CEST) - Logger] Hello
+ * log.warn('Warning blablabl')
+ * log.error('Cannot find this')
+ * log.validate('Ok')
+ */
 export default file => Object.assign(
   log('blue', file),
   {
