025f126ebe481df0ed628bfdd04267e80d4e2346
Guillaume Badi <gbadi@student.42.fr>
2016-06-21 14:30:33 +0200

    auth done for the first websocket for sources

--- src/model/source.js
+++ src/model/source.js
@@ -1,4 +1,11 @@
 
+import logger from '../utils/logger'
+import fetch from 'superagent'
+
+const couchURL = 'http://62.210.236.128:5984'
+
+const log = logger('Source Model')
+
 export const createSource = () => {}
 
 export const deleteSource = () => {}
@@ -6,7 +13,25 @@ export const deleteSource = () => {}
 export const updateSource = () => {}
 
 export const getSource = {
-  byToken () {},
+  byToken (token) {
+    return new Promise((resolve, reject) => {
+
+      fetch
+        .get(`${couchURL}/sources/_design/application/_view/by-token?key=${token}`)
+        .end((err, result) => {
+          if (err) {
+            log.error(err.message)
+            return reject(err)
+          }
+
+          log(`CouchDB resolved with: ${result.statusCode}`)
+
+          resolve(JSON.parse(result.text).rows[0].value)
+        })
+    })
+  },
+
   byID () {},
+
   byName () {},
 }
--- src/server/authenticate.js
+++ src/server/authenticate.js
@@ -1,30 +1,20 @@
 
 import logger from '../utils/logger'
 import { authError } from '../utils/error'
-import fetch from 'superagent'
+import { getSource } from '../model/source'
 
 const log = logger('Authenticate')
-const couchURL = 'http://62.210.236.128:5984'
 
-export default req => new Promise((resolve, reject) => {
+export default req => {
 
   const query = req.resourceURL.query
 
   if (!query.token) {
     log.error('No token found in query')
-    return reject(authError('No token Found in query'))
+    return Promise.reject(authError('No token Found in query'))
   }
 
-  fetch
-    .get(`${couchURL}/sources/_design/application/_view/by-token?key=${query.token}`)
-    .end((err, result) => {
-      if (err) {
-        log.error(err.message)
-        return reject(err)
-      }
+  log('Retrieving Source...')
 
-      log(`CouchDB resolved with: ${result.statusCode}`)
-
-      resolve(JSON.parse(result.text).rows[0].value)
-    })
-})
+  return getSource.byToken(query.token)
+}
